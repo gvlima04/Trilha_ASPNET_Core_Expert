a5a245f3efaaee24ac5a48441de61798080b7dbf
gvlima04 <guilhermevlima04@outlook.com>
2022-06-27 21:35:33 -0300

    Data annotations

--- 02_-_Dominando_o_ASPNET_MVC_Core/MVC-Demo/Models/Movie.cs
+++ 02_-_Dominando_o_ASPNET_MVC_Core/MVC-Demo/Models/Movie.cs
@@ -5,11 +5,27 @@ namespace MVC_Demo.Models
 {
     public class Movie
     {
+        [Key]
         public int Id { get; set; }
-        public String Title { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório"), Display(Name = "Título")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do título deve estar entre 3 e 60 caracteres")]
+        public string? Title { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Formato inválido"), Required(ErrorMessage = "Campo obrigatório")]
+        [Display(Name = "Lançamento")]
         public DateTime ReleaseDate { get; set; }
-        public string Gender { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\u00C0-\u00FF""'\w-]*$", ErrorMessage = "Formato inválido")]
+        [Display(Name = "Gênero"), Required(ErrorMessage = "Campo obrigatório")]
+        public string? Gender { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Valor de 1 a 1000")]
+        [Display(Name = "Valor"), Required(ErrorMessage = "Campo obrigatório")]
         public decimal Value { get; set; }
+
+        [RegularExpression(@"^[0-5]*$", ErrorMessage = "Somente números de 0 - 5")]
+        [Display(Name = "Avaliação"), Required(ErrorMessage = "Campo obrigatório")]
         public int Rating { get; set; }    
     
     }
